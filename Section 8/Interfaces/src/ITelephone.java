2f1507620b66fb7d43dcec7bc38f08d897ed26d0
Arthur Reimus <arthurreimus1623@gmail.com>
2022-08-11 15:09:03 +0800

    Move files to section 9

--- Section 8/Interfaces/src/ITelephone.java
+++
@@ -1,9 +0,0 @@
-public interface ITelephone {
-//    Methods
-//    for a class that implements this interface, it must implement these methods
-     void powerOn();
-     void dial(int phoneNumber);
-     void answer();
-     boolean callPhone(int phoneNumber);
-     boolean isRinging();
-}
